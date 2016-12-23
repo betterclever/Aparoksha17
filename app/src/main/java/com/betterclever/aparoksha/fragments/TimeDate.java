0ae06998f92e5ec519f19bb75ce6d730a5954833
betterclever <paliwal.pranjal83@gmail.com>
2016-12-23 20:45:12 +0530

    Added Time and Location Card

---
+++ app/src/main/java/com/betterclever/aparoksha/fragments/TimeDate.java
@@ -0,0 +1,38 @@
+package com.betterclever.aparoksha.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.betterclever.aparoksha.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimeDate extends Fragment {
+
+
+    public TimeDate() {
+    }
+
+    public static TimeDate newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        TimeDate fragment = new TimeDate();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_time_date, container, false);
+    }
+
+}
